3bc088ad5d4a526afe1c93169cb8dbad01de26e9
Marco Brack <PapstDonB@Googlemail.com>
2024-10-08 16:06:57 +0200

    Follow system preference for dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,23 @@ import "@fontsource/roboto/700.css";
 import "./styles.css";
 import { Router } from "./routing";
 import { Container } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
 export default function App() {
   return (
-    <Container maxWidth="xs" sx={{ pt: 2, pb: 8 }}>
-      <Router />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="xs" sx={{ pt: 2, pb: 8 }}>
+        <Router />
+      </Container>
+    </ThemeProvider>
   );
 }
